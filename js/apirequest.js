d737441221a34265d2d91227d7e3928e5bf79515
Fran_Jimenez <fjimenezjob@gmail.com>
2020-06-02 17:20:10 +0200

    Script comentado

--- js/apirequest.js
+++ js/apirequest.js
@@ -12,25 +12,28 @@ const URL_API = "https://rickandmortyapi.com/api/";
 const PERSONAJE = "character/:id";
 const OPCIONES = { crossDomain : true };
 
+// Arrow function que imprime la frase del personaje
 const imprimir = objeto => document.write(`<p class="main-article-contenido">Hola mi nombre es ${objeto.name}, mi especie es ${objeto.species}, provengo de ${objeto.origin['name']}.</p><br><br>`);
 
 
 function getPersonaje(id){
+    // Hace la peticion a la API y envia el resultado a la funcion imprimir para que imprima la frase
     const URL = `${URL_API}${PERSONAJE.replace(':id', id)}`;
     $.get(URL, OPCIONES, imprimir);
 }
 
+// Establecemos un array donde se van a cargar los ids de los personajes a consultar
 var personajesRq = [];
 do{
-    var preguntarId = prompt('Introduce la id del personaje, cuando quieras consultar escribe:  consultar');
-
+    var preguntarId = prompt('Introduce las ids de los personajes que desees, cuando quieras consultar escribe:  consultar');
+    // Cuando el usuario introduzca 'consultar' la palabra no se añadirá al array de personajesRq
     if(preguntarId !== 'consultar'){
         personajesRq.push(preguntarId);
     }
-
+    // El do while se para cuando introducimos : 'consultar' en el prompt
 } while(preguntarId !== 'consultar');
 
-
+// por cada id en el array de personajesRq llamamos a la funcion para que pida la información de cada uno
 for (id of personajesRq){
     getPersonaje(id);
 }
